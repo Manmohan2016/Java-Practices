306a43ab0aa134bd855a1844f39d8f8de8514e0e
Manmohan2016 <manmohan.tutree@gmail.com>
2025-04-02 18:37:16 +0530

    Implemented dropdown

--- src/test/java/adminPortal.java
+++ src/test/java/adminPortal.java
@@ -6,7 +6,8 @@
 	import org.openqa.selenium.WebElement;
 	import org.openqa.selenium.chrome.ChromeDriver;
 	import org.openqa.selenium.support.ui.ExpectedConditions;
-	import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 	public class adminPortal {
 
@@ -43,8 +44,29 @@
 	        WebElement viewAllButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/user-status?type=Active']//span[contains(text(),'View all')]")));
 	        viewAllButton.click();
 	        Thread.sleep(5000);
+	    /*    WebElement dropdownElement = driver.findElement(By.id("demo-simple-select"));
+	        Select dropdown = new Select(dropdownElement);
+	        dropdown.selectByVisibleText("25");    */
+	        
+	     // ✅ Click the dropdown to open options
+	        WebElement dropdownElement = driver.findElement(By.id("demo-simple-select"));
+	        dropdownElement.click();
+
+	        // ✅ Wait for the option to appear and select "25"
+	        WebElement option25 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[normalize-space()='25']")));
+	        option25.click();
+	        
 	        driver.findElement(By.xpath("//input[@placeholder='Search Here…']")).sendKeys("amanfordeenbytes@gmail.com");
 	        Thread.sleep(5000);
+	        
+	        driver.findElement(By.xpath("//div[@class='d-block']//span[contains(text(),'Courses')]")).click();
+	        driver.findElement(By.xpath("//div[@id='root']//div[@class='MuiCollapse-root MuiCollapse-vertical MuiCollapse-entered css-1cbf1l2']//a[1]")).click();
+	        driver.findElement(By.xpath("//a[@href=\"/courses/add-course\"]")).click();
+	        Thread.sleep(2000);
+	        WebElement select = driver.findElement(By.xpath("//select[@id=\"teacherId\"]"));
+	        Select dropdown = new Select(select);
+	        dropdown.selectByVisibleText("Shadi Alsuleiman");
+	        Thread.sleep(2000);
 
 	        // ✅ Close the browser
 	        driver.quit();
